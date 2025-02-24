657a4bb446ea7135c5ac1d441b567e84cb5091b3
Terufuji3 <terufuji.med@gmail.com>
2025-02-25 03:10:24 +0900

    vault backup: 2025-02-25 03:10:23

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "teru Blog",
+    pageTitle: "teru notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
